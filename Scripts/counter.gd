f750320d261675b0f6e65e63246acbb1f4b4e9cf
TARDIS-Maker <mattheaslboelter@gmail.com>
2018-01-15 19:32:03 -0500

    Basic clickable setup.
    Objects are removed from scene but not reported.
    
    Clickable is it's own scene.

---
+++ Scripts/counter.gd
@@ -0,0 +1,10 @@
+var count
+
+func _init():
+	count = 0
+	
+func increment():
+	count = count + 1
+	print(count)
+
+
--- Scripts/pickable.gd
+++ Scripts/pickable.gd
@@ -1,10 +1,12 @@
-
 extends Area2D
 
+var counter = preload("counter.gd").new()
+
 func _input_event(viewport, event, shape_idx):
-    if event.type == InputEvent.MOUSE_BUTTON \
-    and event.button_index == BUTTON_LEFT \
-    and event.pressed:
-        print("Clicked")
+	if event.type == InputEvent.MOUSE_BUTTON \
+	and event.button_index == BUTTON_RIGHT \
+	and event.pressed:
+		counter.increment()
+		self.get_parent().queue_free()
 
 
